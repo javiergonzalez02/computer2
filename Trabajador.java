6650194af2599f49ccc01dcd6a7e7de3d6a091bd
IvanSD <ivansd122015@gmail.com>
2023-12-01 13:49:10 +0100

    Commit 4: resto de la segunda clase

--- Trabajador.java
+++ Trabajador.java
@@ -20,4 +20,22 @@ public class Trabajador {
     public Productos getProducto() {
         return producto;
     }
+    
+    public void cambiarNombreTrabajador(String nuevoNombreTrabajador){
+        nombreTrabajador = nuevoNombreTrabajador;
+    }
+    
+    public void cambiarCantidadProduccion(int nuevaCantidadProduccion){
+        cantidadProduccion = nuevaCantidadProduccion;
+    }
+    
+    public void cambiarPlacaBase(Productos nuevoProducto){
+        producto = nuevoProducto;
+    }
+
+     public void imprimirDetalles(){
+        System.out.println("Nombre Trabajador: "+ nombreTrabajador + ", Produccion: " + cantidadProduccion + 
+        ", Producto: " + producto.getNombreProducto() + ", Cantidad: " + producto.getCantidadProducto() );
+    }
+    
 }
